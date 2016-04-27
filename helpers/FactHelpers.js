c2b481a122f36c95f061541e8b91bbdb08630e70
Amir Ashtiani <pillowmermaid@gmail.com>
2016-04-27 01:09:57 -0400

    Fix image urls

--- helpers/FactHelpers.js
+++ helpers/FactHelpers.js
@@ -1,7 +1,7 @@
 import { addImage, addFact } from '../actions'
 
 const factProxy = '/catfacts'
-const imgURL = 'http://thecatapi.com/api/images/get?formal=xml&results_per_page=10'
+const imgURL = 'https://thecatapi.com/api/images/get?formal=xml&results_per_page=10'
 const backupIMG = 'https://c4.staticflickr.com/8/7172/6508022985_b22200ced0_z.jpg'
 
 export const buildFact = (dispatch) => {
